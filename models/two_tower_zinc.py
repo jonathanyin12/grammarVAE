a26b30b7e149d880f6c0fc03a197d65ca6e9c5c7
Jonathan Yin <jonathanyin.12@gmail.com>
2020-08-05 13:53:18 -0400

    Update two_tower_zinc.py

--- models/two_tower_zinc.py
+++ models/two_tower_zinc.py
@@ -83,7 +83,7 @@ class MoleculeVAE():
             #                          loss={'decoded_mean': vae_loss, 'decoded_mean_2': vae_loss})
             # self.autoencoder.optimizer.set_weights(optimizer_weights)
         else:
-            self.autoencoder.compile(optimizer=Adam(learning_rate=5e-4), experimental_run_tf_function=False,
+            self.autoencoder.compile(optimizer=Adam(learning_rate=5e-4),
                                      loss={'decoded_mean': vae_loss, 'decoded_mean_2': vae_loss})
 
     # Encoder tower structure
